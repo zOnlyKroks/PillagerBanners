4726422a14079c64179fc6ccd56d07ac0f8b9f2e
zOnlyKroks <finnrades@gmail.com>
2023-11-15 21:49:43 +0100

    -Remove unneccesary translations -Bump version -Add author and description -Remove uneccesary logging

--- src/main/java/de/zonlykroks/pillagerbanners/PillagerBanners.java
+++ src/main/java/de/zonlykroks/pillagerbanners/PillagerBanners.java
@@ -3,20 +3,15 @@ package de.zonlykroks.pillagerbanners;
 import de.zonlykroks.pillagerbanners.config.PillagerBannersConfig;
 import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.DedicatedServerModInitializer;
-import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
-import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.minecraft.entity.*;
-import net.minecraft.entity.mob.PillagerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
-import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.hit.HitResult;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,16 +27,11 @@ public class PillagerBanners implements DedicatedServerModInitializer {
         UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
             List<String> allowedEntitiesIdentifiers = PillagerBannersConfig.allowedEntities;
 
-            System.out.println(allowedEntitiesIdentifiers);
-            System.out.println(entity.getType().getUntranslatedName());
-
             if(allowedEntitiesIdentifiers.contains(entity.getType().getUntranslatedName()) && entity instanceof LivingEntity livingEntity && hand == Hand.MAIN_HAND && !world.isClient && hitResult != null) {
                 List<String> allowedItemsIdentifiers = PillagerBannersConfig.allowedItems;
                 List<Item> items = new ArrayList<>();
 
-                allowedItemsIdentifiers.forEach(s -> {
-                    items.add(Registries.ITEM.get(Identifier.tryParse(s)));
-                });
+                allowedItemsIdentifiers.forEach(s -> items.add(Registries.ITEM.get(Identifier.tryParse(s))));
 
                 ItemStack mainHandStack = player.getInventory().getMainHandStack();
 
